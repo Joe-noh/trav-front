a0ebb2dd3b30c889cddaf39a611a270990a87de9
Joe-noh <goflb.jh@gmail.com>
2016-04-11 22:15:46 +0900

    introduce webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,14 @@ module.exports = {
   output: {
     path: __dirname + "/public",
     filename: "bundle.js"
+  },
+
+  devServer: {
+    contentBase: "./public",
+    port: 8080,
+    colors: true,
+    historyApiFallback: true,
+    inline: true
   }
 }
 
