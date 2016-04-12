3256cceb453b3aedd7174188e865a81f8bdf6cbe
Joe-noh <goflb.jh@gmail.com>
2016-04-12 22:37:31 +0900

    use browserHistory to receive callback params

--- app/main.js
+++ app/main.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, hashHistory} from 'react-router';
+import {Router, Route, browserHistory} from 'react-router';
 import Greeter from './Greeter.js';
 
 import './main.styl';
 
 let routes = (
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={Greeter}/>
   </Router>
 );
