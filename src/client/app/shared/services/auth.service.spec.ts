cb3522198dfc9e2cad654be209b7b5a80d3f4d8d
Joe-noh <goflb.jh@gmail.com>
2016-05-01 10:59:35 +0900

    test AuthService's fetchXxxx functions

--- src/client/app/shared/services/auth.service.spec.ts
+++ src/client/app/shared/services/auth.service.spec.ts
@@ -1,33 +1,67 @@
 import {
   beforeEachProviders,
+  beforeEach,
   it,
-  iit,
   describe,
-  ddescribe,
   expect,
   inject,
-  async
 } from 'angular2/testing';
 
 import {provide} from 'angular2/core';
 import {AuthService} from './auth.service';
 import {StorageService} from './storage.service';
-import {Http} from 'angular2/http';
-
-class MockHttp {}
+import {Http, HTTP_PROVIDERS, Response, ResponseOptions} from 'angular2/http';
+import {Observable} from "rxjs/Rx";
 
 class MockStorage {}
+
+let service, http;
 
 describe('Auth Service', () => {
 
   beforeEachProviders(() => [
     AuthService,
-    provide(Http, {useClass: MockHttp}),
+    HTTP_PROVIDERS,
     provide(StorageService, {useClass: MockStorage})
   ]);
 
-  it('should ...', inject([AuthService], (service: AuthService) => {
-
+  beforeEach(inject([AuthService, Http], (_service, _http) => {
+    service = _service;
   }));
 
+  describe('fetchSigninUrl()', () => {
+    let url = "https://twitter.com/oauth";
+
+    beforeEach(inject([Http], (_http) => {
+      http = _http;
+
+      spyOn(http, 'get').and.returnValue(Observable.from([
+        new Response(new ResponseOptions({body: {url}}))
+      ]));
+    }));
+
+    it('returns twitter signin url', () => {
+      service.fetchSigninUrl().subscribe(data => {
+        expect(data).toEqual({url});
+      });
+    });
+  });
+
+  describe('fetchAuthToken()', () => {
+    let token = "hogehogefugafuga";
+
+    beforeEach(inject([Http], (_http) => {
+      http = _http;
+
+      spyOn(http, 'post').and.returnValue(Observable.from([
+        new Response(new ResponseOptions({body: {token}}))
+      ]));
+    }));
+
+    it('returns JWT for auth', () => {
+      service.fetchAuthToken().subscribe(data => {
+        expect(data).toEqual({token});
+      });
+    });
+  });
 });
