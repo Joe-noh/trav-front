bb79b19774d42c61c9e910808cf3397e6d78560b
Joe-noh <goflb.jh@gmail.com>
2016-05-04 16:27:12 +0900

    fetch trips from backend

--- src/client/app/+trips/trips.component.ts
+++ src/client/app/+trips/trips.component.ts
@@ -1,18 +1,25 @@
 import {Component, OnInit} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 
+import {ApiService} from '../shared';
+
 @Component({
   moduleId: __moduleName,
   selector: 'trips',
   templateUrl: 'trips.component.html',
   styleUrls: ['trips.component.css'],
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ApiService]
 })
 export class TripsComponent implements OnInit {
+  private trips;
 
-  constructor() {}
+  constructor(private api: ApiService) {}
 
   ngOnInit() {
+    this.api.getRequest('/api/trips').subscribe(data => {
+      this.trips = data.trips;
+    });
   }
 
 }
--- src/client/app/shared/index.ts
+++ src/client/app/shared/index.ts
@@ -1,5 +1,6 @@
 export * from './services/storage.service';
 export * from './services/auth.service';
+export * from './services/api.service';
 
 export * from './directives/auth-router-outlet.directive';
 
